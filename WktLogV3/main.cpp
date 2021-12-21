b8923b2a6ccd03cb10ba8f7f7f9357e16b10f723
napolifutbol <harrison_v_cooley@me.com>
2021-12-20 21:35:31 -0500

    Initial Commit

---
+++ WktLogV3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WktLogV3
+//
+//  Created by Harrison Cooley on 12/20/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
